12e550abb75529c26bfe8717fe29b022ed22ec18
Manuel Schulze <manuel_schulze@i-entwicklung.de>
2013-01-03 16:01:36 +0100

    License * Lizenz korrigiert

--- src/main/webapp/static/js/sketchpad/colorchooser.js
+++ src/main/webapp/static/js/sketchpad/colorchooser.js
@@ -1,5 +1,5 @@
 /*
- * Copyright 2013 Manuel Schulze <manuel_schulze@i-entwicklung.de>
+ * Copyright 2012-2013 Manuel Schulze <manuel_schulze@i-entwicklung.de>
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
